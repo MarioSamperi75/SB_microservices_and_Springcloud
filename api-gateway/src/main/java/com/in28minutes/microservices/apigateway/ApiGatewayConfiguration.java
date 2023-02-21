89f685cb46f148f653ba4956496462eca0b097e6
Mario Samperi <mariosamperi@marios-mbp.home>
2023-02-21 21:57:33 +0100

    route operations with Spring Cloud Gateway

--- api-gateway/src/main/java/com/in28minutes/microservices/apigateway/ApiGatewayConfiguration.java
+++ api-gateway/src/main/java/com/in28minutes/microservices/apigateway/ApiGatewayConfiguration.java
@@ -7,17 +7,37 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class ApiGatewayConfiguration {
-	
+
 	@Bean
 	public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
-		
+
 		return builder.routes()
 				.route(p -> p.path("/get")
-					.filters(f -> f
-							.addRequestHeader("MyHeader", "MyURI")
-							.addRequestParameter("Param", "MyValue"))
-					.uri("http://httpbin.org:80"))
+						.filters(f -> f.addRequestHeader("MyHeader", "MyURI")
+						.addRequestParameter("Param", "MyValue"))
+						.uri("http://httpbin.org:80"))
+				
+				
+				// from http://localhost:8765/currency-exchange/currency-exchange/from/USD/to/INR
+				// to http://localhost:8765/currency-exchange/from/USD/to/INR 
+				.route(p -> p.path("/currency-exchange/**")
+						.uri("lb://currency-exchange"))
+				.route(p -> p.path("/currency-conversion/**")
+						.uri("lb://currency-conversion"))
+				.route(p -> p.path("/currency-conversion-feign/**")
+						.uri("lb://currency-conversion"))
+				
+				
+				// from http://localhost:8765/currency-conversion/currency-conversion-feign/from/USD/to/INR/quantity/10
+				// to http://localhost:8765/currency-conversion-new/from/USD/to/INR/quantity/10
+				.route(p -> p.path("/currency-conversion-new/**")
+						.filters(f -> f.rewritePath(
+								"/currency-conversion-new/(?<segment>.*)", 
+								"/currency-conversion-feign/${segment}"))
+						.uri("lb://currency-conversion"))
+
 				.build();
+
 	}
 
 }
