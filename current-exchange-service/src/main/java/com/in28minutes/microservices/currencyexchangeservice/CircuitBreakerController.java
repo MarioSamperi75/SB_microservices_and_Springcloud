1e809b8c7197fcbc63cbc67024374c21af3dd470
Mario Samperi <mariosamperi@marios-mbp.home>
2023-02-23 09:20:15 +0100

    Resilience4j - implementind rate limiter

--- current-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CircuitBreakerController.java
+++ current-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CircuitBreakerController.java
@@ -8,20 +8,25 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 
 @RestController
 public class CircuitBreakerController {
 	
 	private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
+	int counter = 0;
 
 	@GetMapping("/circuit-breaker")
 	//@Retry(name = "circuit-breaker", fallbackMethod = "hardcodedResponse")
-	@CircuitBreaker(name = "default", fallbackMethod = "hardcodedResponse")
+	//@CircuitBreaker(name = "default", fallbackMethod = "hardcodedResponse")
+	@RateLimiter(name="prova")
 	public String dummyApi() {
-		logger.info("Circuit breaker call received");
-		ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/not-working-endpoint", String.class);
-		return forEntity.getBody();
+		//logger.info("Circuit breaker call received");
+		//ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/not-working-endpoint", String.class);
+		//return forEntity.getBody();
+		counter++;
+		return "Testing RateLimter " + counter	;
 	}
 	
 	public String hardcodedResponse (Exception ex) {
